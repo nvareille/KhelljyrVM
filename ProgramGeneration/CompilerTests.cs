e61997dc0eac42a038f085d1ae611a1ea5ceae97
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2020-05-30 14:45:25 +0200

    Add unit tests support

--- ProgramGeneration/CompilerTests.cs
+++ ProgramGeneration/CompilerTests.cs
@@ -50,37 +50,37 @@ namespace ProgramGeneration
             return (p.Run());
         }
         
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicAdd()
         {
            Assert.AreEqual(84, TestFile("Samples/BasicAdd.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void AdvancedAdd()
         {
             Assert.AreEqual(126, TestFile("Samples/AdvancedAdd.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicFct()
         {
             Assert.AreEqual(84, TestFile("Samples/BasicFct.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicFctVar()
         {
             Assert.AreEqual(126, TestFile("Samples/BasicFctVar.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicLoop()
         {
             Assert.AreEqual(10, TestFile("Samples/BasicLoop.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicPreprocessing()
         {
             File.Copy("../../../../Docs/Samples/Defines.txt", "Samples/Defines.txt", true);
@@ -90,43 +90,43 @@ namespace ProgramGeneration
             }));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicCast()
         {
             Assert.AreEqual(43, TestFile("Samples/BasicCast.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicPtr()
         {
             Assert.AreEqual(42, TestFile("Samples/BasicPtr.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void AddFromPtr()
         {
             Assert.AreEqual(42, TestFile("Samples/AddFromPtr.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void AdvancedPtr()
         {
             Assert.AreEqual(42, TestFile("Samples/AdvancedPtr.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicLibCall()
         {
             Assert.AreEqual(126, TestFile("Samples/BasicLibCall.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicStructures()
         {
             Assert.AreEqual(84, TestFile("Samples/BasicStructures.txt"));
         }
 
-        [TestMethod]
+        [TestMethod, TestCategory("Compiler")]
         public void BasicTemplates()
         {
             Assert.AreEqual(85, TestFile("Samples/BasicTemplate.txt"));
